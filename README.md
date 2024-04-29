f262beff7dbc25733ed5567940884813f9893bda
Marlu0 <marcel.manzano01@estudiant.upf.edu>
2024-04-29 23:30:39 +0200

    A

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Group members:
 
 Project design:
 #put project design here
+
+Tasklist:
+
+- Decide Scenarios and Narrative
+- Implement a Menu with UI that lets the user:
+    - Start New Game
+    - Create Character
+- Structure of game without scenario navigation:
+    - Dialogs
+    - Cutscenes
+    - Selection menus
--- game.c
+++ game.c
@@ -1,1 +1,38 @@
 // Here we implement the functions required to play the game, including scenario navigation and turn based combat
+
+#include <stdio.h>
+
+int selection(char *strings[]){
+
+    int n = sizeof(strings) / sizeof(strings[0]);
+    int selection = 0;
+
+    /* While loop to handle if selection is invalid */
+    while (selection < 1 || selection > n) {
+
+        /* Printing the possible selections */
+        for (int i = 0; i < n; i++) {
+            printf("%d. %s\n", i + 1, strings[i]);
+        }
+
+        /* Getting the selection */
+        printf("Enter your selection (1-%d): ", n);
+        scanf("%d", &selection);
+
+        /* Flush the input buffer to clear any remaining characters */
+        while (getchar() != '\n');
+
+        /* Print invalid selection if input doesn't belong to [1, n] */
+        if (selection < 1 || selection > n) {
+            printf("Invalid selection. Please try again.\n");
+        }
+    };  
+    return selection;
+}
+
+int main() {
+    char *strings[] = {"Hello", "World", "How", "Are", "You"};
+    selection(strings);
+    printf("You selected: %d\n", selection);
+    return 0;
+}
