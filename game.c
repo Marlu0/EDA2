3bc9bd4699a9e269fe865a95584296b9ab4c68d6
Marlu0 <marcel.manzano01@estudiant.upf.edu>
2024-04-30 08:55:27 +0200

    Added get_selection and main title

--- game.c
+++ game.c
@@ -1,14 +1,18 @@
-// Here we implement the functions required to play the game, including scenario navigation and turn based combat
-
 #include <stdio.h>
+#include <stdbool.h>
 
-int selection(char *strings[]){
+int get_selection(const char *strings[]) {
+    int selection;
+    int n = 0;
 
-    int n = sizeof(strings) / sizeof(strings[0]);
-    int selection = 0;
-
-    /* While loop to handle if selection is invalid */
-    while (selection < 1 || selection > n) {
+    /* Count the number of strings until NULL terminator */
+    while (strings[n] != NULL) {
+        n++;
+    }
+    /* Create a flag to track validity of input */
+    bool valid_input = false;
+    
+    while (!valid_input) {
 
         /* Printing the possible selections */
         for (int i = 0; i < n; i++) {
@@ -17,22 +21,64 @@ int selection(char *strings[]){
 
         /* Getting the selection */
         printf("Enter your selection (1-%d): ", n);
-        scanf("%d", &selection);
-
-        /* Flush the input buffer to clear any remaining characters */
-        while (getchar() != '\n');
-
-        /* Print invalid selection if input doesn't belong to [1, n] */
-        if (selection < 1 || selection > n) {
-            printf("Invalid selection. Please try again.\n");
+        
+        if (scanf("%d", &selection) != 1) {
+            printf("Invalid input. Please enter a number.\n");
+            // Flush the input buffer to clear any remaining characters
+            while (getchar() != '\n');
+        } else {
+            valid_input = true;
         }
-    };  
+
+        if (valid_input && (selection < 1 || selection > n)) {
+            printf("Invalid selection. Please try again.\n");
+            valid_input = false;
+        }
+    };
+    
     return selection;
 }
 
+void print_main_title() {
+    printf("___________________________________________________________________________\n");
+    printf(" _     _                _          _        _     _                          \n");
+    printf("| |   (_)_________   _ ( )___     / \\   ___(_) __| |                         \n");
+    printf("| |   | |_  /_  / | | ||// __|   / _ \\ / __| |/ _` |                         \n");
+    printf("| |___| |/ / / /| |_| |  \\__ \\  / ___ \\ (__| | (_| |                         \n");
+    printf("|_____|_/___/___|\\__, |  |___/ /_/   \\_\\___|_|\\__,_|_         _   _          \n");
+    printf("   / \\   __| |_  |___/_ _ __ | |_ _   _ _ __ ___  (_)_ __   | |_| |__   ___ \n");
+    printf("  / _ \\ / _` \\ \\ / / _ \\ '_ \\| __| | | | '__/ _ \\ | | '_ \\  | __| '_ \\ / _ \\ \n");
+    printf(" / ___ \\ (_| |\\ V /  __/ | | | |_| |_| | | |  __/ | | | | | | |_| | | |  __/\n");
+    printf("/_/   \\_\\__,_| \\_/ \\___|_| |_|\\__|\\__,_|_|  \\___| |_|_| |_|  \\__|_| |_|\\___|\n");
+    printf("▄▄▌ ▐ ▄▌▪  ▄▄▌  ·▄▄▄▄      ▄▄▌ ▐ ▄▌▄▄▄ ..▄▄ · ▄▄▄▄▄\n");
+    printf("██· █▌▐███ ██•  ██▪ ██     ██· █▌▐█▀▄.▀·▐█ ▀. •██  \n");
+    printf("██▪▐█▐▐▌▐█·██▪  ▐█· ▐█▌    ██▪▐█▐▐▌▐▀▀▪▄▄▀▀▀█▄ ▐█.▪\n");
+    printf("▐█▌██▐█▌▐█▌▐█▌▐▌██. ██     ▐█▌██▐█▌▐█▄▄▌▐█▄▪▐█ ▐█▌·\n");
+    printf(" ▀▀▀▀ ▀▪▀▀▀.▀▀▀ ▀▀▀▀▀•      ▀▀▀▀ ▀▪ ▀▀▀  ▀▀▀▀  ▀▀▀ \n");
+    printf("___________________________________________________________________________\n\n");
+}
+
 int main() {
-    char *strings[] = {"Hello", "World", "How", "Are", "You"};
-    selection(strings);
-    printf("You selected: %d\n", selection);
+    print_main_title();
+    const char *strings[] = {"New Game", "Character Creation", "Exit", "Credits", NULL}; // NULL terminator
+    int user_selection = get_selection(strings);
+    
+    switch (user_selection) {
+        case 1:
+            printf("You selected: %s\n", strings[user_selection - 1]);
+            break;
+        case 2:
+            printf("You selected: %s\n", strings[user_selection - 1]);
+            break;
+        case 3:
+            printf("You selected: %s\n", strings[user_selection - 1]);
+            break;
+        case 4:
+            printf("You selected: %s\n", strings[user_selection - 1]);
+            break;
+        default:
+            printf("Invalid selection\n");
+    }
+
     return 0;
 }
