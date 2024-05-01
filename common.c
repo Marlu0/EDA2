895cb74081ce526aa6b7a4e172f4ce15f6a408fb
Marlu0 <marcel.manzano01@estudiant.upf.edu>
2024-05-01 19:32:58 +0200

    A

--- common.c
+++ common.c
@@ -71,21 +71,3 @@ void print_main_title() {
     printf("                              ▀▀▀▀ ▀▪▀▀▀.▀▀▀ ▀▀▀▀▀•      ▀▀▀▀ ▀▪ ▀▀▀  ▀▀▀▀  ▀▀▀ \n");
     printf("_____________________________________________________________________________________\n\n");
 }
-
-/*
-This function recieves:
-    - Nothing
-It does:
-    - Prints the main menu
-Returns:
-    - Integer that represents selection
-*/
-int main_menu(){
-    
-    /* First we print the main title */
-    print_main_title();
-
-    /* Then we use the get_selection function to display the options and manage the selection */
-    const char *options[] = {"New Game","Create Character","Credits","Exit",NULL /* NULL terminator to mark the end of the array */};
-    return get_selection(options);
-}
