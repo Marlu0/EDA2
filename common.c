85f35b9bd896a8fe4ecabbc0d35b7d725d33108a
Marlu0 <marcel.manzano01@estudiant.upf.edu>
2024-05-01 19:30:53 +0200

    Programmed main_menu()

--- common.c
+++ common.c
@@ -78,12 +78,14 @@ This function recieves:
 It does:
     - Prints the main menu
 Returns:
-    - Nothing
+    - Integer that represents selection
 */
-void print_main_menu(){
+int main_menu(){
     
     /* First we print the main title */
     print_main_title();
 
-    /* Then we use the get_selection function to display and recieve the */
+    /* Then we use the get_selection function to display the options and manage the selection */
+    const char *options[] = {"New Game","Create Character","Credits","Exit",NULL /* NULL terminator to mark the end of the array */};
+    return get_selection(options);
 }
