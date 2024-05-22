016cfc4da589978f3c1439faefa670d7575682b4
Pol Obrador <janpol.obrador01@estudiant.upf.edu>
2024-05-22 11:41:15 +0200

    ,mn

--- test_combat.c
+++ test_combat.c
@@ -3,31 +3,9 @@
 #include "combat.h"
 
 
-#define UP 1
-#define LEFT 2
-#define RIGHT 3
-#define DOWN 4
-#define BACKWARDS 5
-// its arranged in that if you do backwards - direction you get the other direction
-
-#define NUM_SKILLS 2
-#define NUM_MODIFIERS 4
-#define MAX_CHOICES 4
-#define MAX_STRING_LEN 100
-#define NUM_OPTIONS 2
-#define NUM_DECISIONS 2
-#define INVENTORY_SIZE 8
-#define AGGRESSIVE_MODE_ACTIVATOR 2.5
-
 
 
 int main(){
-
-
-
-
-
-
     Character jose;
     bool boolean = false;
     jose = create_character();
