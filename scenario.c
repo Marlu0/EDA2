e59b11401e29cf71295a21002b43ee81c7d4e685
Marlu <marcel.manzano01@estudiant.upf.edugit config --global user.email>
2024-05-29 15:49:05 +0200

    Fixed text order

--- scenario.c
+++ scenario.c
@@ -59,10 +59,10 @@ Scenario *init_scenario_list(Decision decision_list[], Character *character){ //
 
     *temp++ = (Scenario){"TURTLE'S TOMBSTONES", print_description3, "This dark old shop smell rotten", .decision = decision_list[2], .numEnemies = 0, .completed = false,};
     
-    *temp++ = (Scenario){"SHERIFF BULL'S STATION", print_description4, "You can hear the paperwork being done, its quiet and tense", .decision = decision_list[4], .completed = false, .numEnemies = 0};
+    *temp++ = (Scenario){"SHERIFF BULL'S STATION", print_description4, "You shouldn't be here, you just murdered this man", .decision = decision_list[4], .completed = false, .numEnemies = 0};
 
-    *temp++ = (Scenario){"SHERIFF BULL'S STATION", print_description4, "You shouldn't be here, you just murdered this man", .decision = decision_list[3], .completed = false, .enemies = &enemies[1], .numEnemies = 1};
-     
+    *temp++ = (Scenario){"SHERIFF BULL'S STATION", print_description4, "You can hear the paperwork being done, its quiet and tense", .decision = decision_list[3], .completed = false, .enemies = &enemies[1], .numEnemies = 1};
+
     *temp++ = (Scenario){"TURTLE'S TOMBSTONES", print_description6, "Turtles body is still on the ground, cold as ice", .decision = decision_list[5], .completed = false, .enemies = &enemies[2]};
 
     *temp++ = (Scenario){"CASINO", print_description7, "You got your water", .decision = decision_list[6], .completed = false, .enemies = &enemies[3], .numEnemies = 1};
